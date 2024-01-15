69d5045cca97805c7d7483d6ba7df9d184d34984
wali  ahmad <waliahmadfiles@gmail.com>
2024-01-15 15:56:01 +0500

    configure next auth

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,31 @@
+import NextAuth from "next-auth/next";
+import GoogleProvider from "next-auth/providers/google";
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  secret: "vxcvxvertwrwerwe",
+  callbacks: {
+    async jwt({ token, account }, user) {
+      if (account) {
+        token = Object.assign({}, token, {
+          access_token: account.id_token,
+        });
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (session) {
+        session = Object.assign({}, session, {
+          access_token: token.access_token,
+        });
+      }
+      return session;
+    },
+  },
+});
+
+export { handler as GET, handler as POST };
