f285aa4bc1879aafcb46dfdebc635caec4d90145
wali  ahmad <waliahmadfiles@gmail.com>
2024-07-04 22:18:42 +0500

    change folder structure

---
+++ src/components/shared/Input.tsx
@@ -0,0 +1,33 @@
+import * as React from "react";
+
+import { cn } from "@/lib/utils";
+
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  error?: boolean;
+  helperText?: string;
+}
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, error, helperText, ...props }, ref) => {
+    return (
+      <>
+        <input
+          type={type}
+          className={cn(
+            `text-black flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 ${
+              error ? "border-red-500" : ""
+            }`,
+            className
+          )}
+          ref={ref}
+          {...props}
+        />
+        {error && <p className="text-xs text-red-500 py-1">{helperText}</p>}
+      </>
+    );
+  }
+);
+Input.displayName = "Input";
+
+export { Input };
